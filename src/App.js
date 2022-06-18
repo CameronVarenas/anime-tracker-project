a8463441cdd8235e74306b9e1f8b953005a1b7cd
Cameron Varenas <camerontvarenas@gmail.com>
2022-06-18 16:52:38 -0400

    created class component in App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div>
-
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
 }
 
 export default App;
